2f58080c657a7365a27de8af042a7e248d7578f9
XI64 <ruiaki@protonmail.com>
2021-02-19 17:41:07 +0530

    Changed Hash gen

--- src/main/scala/prop/settings.scala
+++ src/main/scala/prop/settings.scala
@@ -7,7 +7,7 @@ object settings {
   val backgroundColor = new Color(240, 240, 240)
   val width      = 300
   val saturation = 0.45f
-  val value      = 0.80f
+  val brightness = 0.80f
   val username = "<put-username-here>"
   val filename = "<put-file-name-here>"
 }
